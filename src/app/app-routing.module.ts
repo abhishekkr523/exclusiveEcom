1410099735fc91253555bbb99ed3ddedcf088be8
abhishekkr523 <7033abhishek@gmail.com>
2024-10-04 05:48:46 +0530

    design carousel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarCarouselComponent } from './components/main/sidebar-carousel/sidebar-carousel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'SidebarCarousel', component: SidebarCarouselComponent},
+  {path: '**', redirectTo: '404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
