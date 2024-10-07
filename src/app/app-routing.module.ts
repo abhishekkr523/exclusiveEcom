86442941d220da523219d2dc7206c8681bc8989b
abhishekkr523 <7033abhishek@gmail.com>
2024-10-07 10:45:25 +0530

    merge all beanches changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,19 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: '**', redirectTo: '404'}
 ];
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: 'home',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: 'home',
+//     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+//   },
+//   { path: '**', redirectTo: '404' }  // Keep this to handle unknown paths
+// ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,12 +5,19 @@ import { HomeComponent } from './home.component';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { RouterModule, Routes } from '@angular/router';
 
-
+// Define the routes for HomeComponent
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: HomeComponent // HomeComponent will be loaded for /home
+//   }
+// ];
 @NgModule({
   declarations: [HomeComponent,BannerComponent,CarouselComponent],
   imports: [
-    CommonModule,CarouselModule
+    CommonModule,CarouselModule,
   ],
   exports : [
     BannerComponent,
